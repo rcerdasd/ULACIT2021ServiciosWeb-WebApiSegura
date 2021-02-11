67b239e4a2b1a423b5a7b30b66a9a02f0c9d92fa
rcerdasd418 <rcerdasd@ulacit.ed.cr>
2021-02-10 20:41:37 -0600

    controllers

---
+++ WebApiSegura/Models/Habitacion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiSegura.Models
+{
+    public class Habitacion
+    {
+        public int HAB_CODIGO { get; set; }
+        public int HOT_CODIGO { get; set; }
+        public int HAB_NUMBERO { get; set; }
+        public int HAB_CAPACIDAD { get; set; }
+        public int HAB_TIPO { get; set; }
+        public string HAB_DESCRIPCION { get; set; }
+        public string HAB_ESTADO { get; set; }
+        public decimal  HAB_PRECIO { get; set; }
+    }
+}
---
+++ WebApiSegura/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiSegura.Models
+{
+    public class Usuario
+    {
+        public int USU_CODIGO { get; set; }
+        public string USU_IDENTIFICACION { get; set; }
+
+        public string USU_NOMBRE { get; set; }
+
+        public string USU_PASSWORD { get; set; }
+        public string USU_EMAIL { get; set; }
+        public string USU_ESTADO { get; set; }
+        public string USU_FECH_NAC { get; set; }
+        public string USU_TELEFONO { get; set; }
+
+    }
+}
