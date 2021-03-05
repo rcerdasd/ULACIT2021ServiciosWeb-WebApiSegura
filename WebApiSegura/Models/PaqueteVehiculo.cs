1e4ca36b4323708e6ec8b53d511b439a1a2d2cab
rcerdasd418 <rcerdasd@ulacit.ed.cr>
2021-03-04 23:12:23 -0600

    updated

---
+++ WebApiSegura/Models/PaqueteVehiculo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiSegura.Models
+{
+    public class PaqueteVehiculo
+    {
+        public int PAQ_VEH_CODIGO { get; set; }
+        public int VEH_CODIGO { get; set; }
+        public string PAQ_SEGURO { get; set; }
+        public string PAQ_BICICLETA { get; set; }
+        public string PAQ_DESCRIPCION { get; set; }
+    }
+}
---
+++ WebApiSegura/Models/ReservaVehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiSegura.Models
+{
+    public class ReservaVehiculo
+    {
+        public int RES_VEH_CODIGO { get; set; }
+        public int USU_CODIGO { get; set; }
+        public int PAQ_VEH_CODIGO { get; set; }
+        public DateTime RES_VEH_FEC_SALIDA { get; set; }
+        public DateTime RES_VEH_FEC_REGRESO { get; set; }
+
+    }
+}
