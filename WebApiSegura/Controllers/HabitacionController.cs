330c872d4a07493bedbcbbc1f146085b70e79ff5
rcerdasd418 <rcerdasd@ulacit.ed.cr>
2021-03-05 19:41:21 -0600

    Controllers de habitacion y reserva hotel

--- WebApiSegura/Controllers/HabitacionController.cs
+++ WebApiSegura/Controllers/HabitacionController.cs
@@ -4,10 +4,207 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiSegura.Models;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebApiSegura.Controllers
 {
+    [Authorize]
+    //[AllowAnonymous]
+    [RoutePrefix("api/habitacion")]
     public class HabitacionController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Habitacion habitacion = new Habitacion();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT HAB_CODIGO, HOT_CODIGO, HAB_NUMERO, HAB_CAPACIDAD, HAB_TIPO, HAB_DESCRIPCION, HAB_ESTADO, HAB_PRECIO FROM HABITACION WHERE HAB_CODIGO = @HAB_CODIGO", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@HAB_CODIGO", id);
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        habitacion.HAB_CODIGO = sqlDataReader.GetInt32(0);
+                        habitacion.HOT_CODIGO = sqlDataReader.GetInt32(1);
+                        habitacion.HAB_NUMERO = sqlDataReader.GetInt32(2);
+                        habitacion.HAB_CAPACIDAD = sqlDataReader.GetInt32(3);
+                        habitacion.HAB_TIPO = sqlDataReader.GetString(4);
+                        habitacion.HAB_DESCRIPCION = sqlDataReader.GetString(5);
+                        habitacion.HAB_ESTADO = sqlDataReader.GetString(6);
+                        habitacion.HAB_PRECIO = sqlDataReader.GetInt32(7);
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+            return Ok(habitacion);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Habitacion> habitaciones = new List<Habitacion>();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT HAB_CODIGO, HOT_CODIGO, HAB_NUMERO, HAB_CAPACIDAD, HAB_TIPO, HAB_DESCRIPCION, HAB_ESTADO, HAB_PRECIO FROM HABITACION", sqlConnection);
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Habitacion habitacion = new Habitacion()
+                        {
+                            HAB_CODIGO = sqlDataReader.GetInt32(0),
+                            HOT_CODIGO = sqlDataReader.GetInt32(1),
+                            HAB_NUMERO = sqlDataReader.GetInt32(2),
+                            HAB_CAPACIDAD = sqlDataReader.GetInt32(3),
+                            HAB_TIPO = sqlDataReader.GetString(4),
+                            HAB_DESCRIPCION = sqlDataReader.GetString(5),
+                            HAB_ESTADO = sqlDataReader.GetString(6),
+                            HAB_PRECIO = sqlDataReader.GetInt32(7)
+                        };
+                        habitaciones.Add(habitacion);
+                    }
+
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+            return Ok(habitaciones);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Habitacion habitacion)
+        {
+            if (habitacion == null)
+                return BadRequest();
+            if (RegistrarHabitacion(habitacion))
+                return Ok(habitacion);
+            else return InternalServerError();
+        }
+        private bool RegistrarHabitacion(Habitacion habitacion)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO HABITACION (HOT_CODIGO, HAB_NUMERO, HAB_CAPACIDAD, HAB_TIPO, HAB_DESCRIPCION, HAB_ESTADO, HAB_PRECIO) VALUES (@HOT_CODIGO, @HAB_NUMERO, @HAB_CAPACIDAD, @HAB_TIPO, @HAB_DESCRIPCION, @HAB_ESTADO, @HAB_PRECIO)", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@HOT_CODIGO", habitacion.HOT_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HAB_NUMERO", habitacion.HAB_NUMERO);
+                sqlCommand.Parameters.AddWithValue("@HAB_CAPACIDAD", habitacion.HAB_CAPACIDAD);
+                sqlCommand.Parameters.AddWithValue("@HAB_TIPO", habitacion.HAB_TIPO);
+                sqlCommand.Parameters.AddWithValue("@HAB_DESCRIPCION", habitacion.HAB_DESCRIPCION);
+                sqlCommand.Parameters.AddWithValue("@HAB_ESTADO", habitacion.HAB_ESTADO);
+                sqlCommand.Parameters.AddWithValue("@HAB_PRECIO", habitacion.HAB_PRECIO);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Habitacion habitacion)
+        {
+            if (habitacion == null)
+                return BadRequest();
+            if (ActualizarHabitacion(habitacion))
+                return Ok(habitacion);
+            else return InternalServerError();
+        }
+
+        private bool ActualizarHabitacion(Habitacion habitacion)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"UPDATE HABITACION 
+                                                       SET 
+                                                           HOT_CODIGO = @HOT_CODIGO, 
+                                                           HAB_NUMERO = @HAB_NUMERO,
+                                                           HAB_CAPACIDAD = @HAB_CAPACIDAD, 
+                                                           HAB_TIPO = @HAB_TIPO,
+                                                           HAB_DESCRIPCION = @HAB_DESCRIPCION,
+                                                           HAB_ESTADO = @HAB_ESTADO,
+                                                           HAB_PRECIO = @HAB_PRECIO
+                                                           WHERE HAB_CODIGO = @HAB_CODIGO", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@HAB_CODIGO", habitacion.HAB_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HOT_CODIGO", habitacion.HOT_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HAB_NUMERO", habitacion.HAB_NUMERO);
+                sqlCommand.Parameters.AddWithValue("@HAB_CAPACIDAD", habitacion.HAB_CAPACIDAD);
+                sqlCommand.Parameters.AddWithValue("@HAB_TIPO", habitacion.HAB_TIPO);
+                sqlCommand.Parameters.AddWithValue("@HAB_DESCRIPCION", habitacion.HAB_DESCRIPCION);
+                sqlCommand.Parameters.AddWithValue("@HAB_ESTADO", habitacion.HAB_ESTADO);
+                sqlCommand.Parameters.AddWithValue("@HAB_PRECIO", habitacion.HAB_PRECIO);
+
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+            if (EliminarHabitacion(id))
+                return Ok();
+            else return InternalServerError();
+        }
+
+        private bool EliminarHabitacion(int id)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"DELETE HABITACION 
+                                                       WHERE HAB_CODIGO = @HAB_CODIGO", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@HAB_CODIGO", id);
+
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
     }
 }
--- WebApiSegura/Controllers/ReservaHotelController.cs
+++ WebApiSegura/Controllers/ReservaHotelController.cs
@@ -4,10 +4,193 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using WebApiSegura.Models;
+using System.Configuration;
+using System.Data.SqlClient;
 
 namespace WebApiSegura.Controllers
 {
+    //[Authorize]
+    [AllowAnonymous]
+    [RoutePrefix("api/reserva-hotel")]
     public class ReservaHotelController : ApiController
     {
+        [HttpGet]
+        public IHttpActionResult GetId(int id)
+        {
+            Reserva resHotel = new Reserva();
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT RES_CODIGO, USU_CODIGO, HAB_CODIGO, RES_FECHA_INGRESO, RES_FECHA_SALIDA FROM RESERVA WHERE RES_CODIGO = @RES_CODIGO", sqlConnection);
+                    sqlCommand.Parameters.AddWithValue("@RES_CODIGO", id);
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        resHotel.RES_CODIGO = sqlDataReader.GetInt32(0);
+                        resHotel.USU_CODIGO = sqlDataReader.GetInt32(1);
+                        resHotel.HAB_CODIGO = sqlDataReader.GetInt32(2);
+                        resHotel.RES_FECHA_INGRESO = sqlDataReader.GetDateTime(3);
+                        resHotel.RES_FECHA_SALIDA = sqlDataReader.GetDateTime(4);
+                    }
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+            return Ok(resHotel);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAll()
+        {
+            List<Reserva> reservas = new List<Reserva>();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+                {
+                    SqlCommand sqlCommand = new SqlCommand(@"SELECT RES_CODIGO, USU_CODIGO, HAB_CODIGO, RES_FECHA_INGRESO, RES_FECHA_SALIDA FROM RESERVA", sqlConnection);
+                    sqlConnection.Open();
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read())
+                    {
+                        Reserva reserva = new Reserva()
+                        {
+                            RES_CODIGO = sqlDataReader.GetInt32(0),
+                            USU_CODIGO = sqlDataReader.GetInt32(1),
+                            HAB_CODIGO = sqlDataReader.GetInt32(2),
+                            RES_FECHA_INGRESO = sqlDataReader.GetDateTime(3),
+                            RES_FECHA_SALIDA = sqlDataReader.GetDateTime(4)
+                        };
+                        reservas.Add(reserva);
+                    }
+
+
+                    sqlConnection.Close();
+                }
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+
+            return Ok(reservas);
+        }
+
+        [HttpPost]
+        public IHttpActionResult Ingresar(Reserva reserva)
+        {
+            if (reserva == null)
+                return BadRequest();
+            if (RegistrarReservaHotel(reserva))
+                return Ok(reserva);
+            else return InternalServerError();
+        }
+        private bool RegistrarReservaHotel(Reserva reserva)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"INSERT INTO RESERVA (USU_CODIGO, HAB_CODIGO, RES_FECHA_INGRESO, RES_FECHA_SALIDA) VALUES (@USU_CODIGO, @HAB_CODIGO, @RES_FECHA_INGRESO, @RES_FECHA_SALIDA)", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@HOT_NOMBRE", reserva.USU_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HOT_EMAIL", reserva.HAB_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HOT_DIRECCION", reserva.RES_FECHA_INGRESO);
+                sqlCommand.Parameters.AddWithValue("@HOT_TELEFONO", reserva.RES_FECHA_SALIDA);
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
+
+        [HttpPut]
+        public IHttpActionResult Actualizar(Reserva reserva)
+        {
+            if (reserva == null)
+                return BadRequest();
+            if (ActualizarReserva(reserva))
+                return Ok(reserva);
+            else return InternalServerError();
+        }
+
+        private bool ActualizarReserva(Reserva reserva)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"UPDATE RESERVA 
+                                                       SET 
+                                                           USU_CODIGO = @USU_CODIGO, 
+                                                           HAB_CODIGO = @HAB_CODIGO,
+                                                           RES_FECHA_INGRESO = @RES_FECHA_INGRESO, 
+                                                           RES_FECHA_SALIDA = @RES_FECHA_SALIDA,
+                                                           HOT_CATEGORIA = @HOT_CATEGORIA
+                                                       WHERE RES_CODIGO = @RES_CODIGO", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@RES_CODIGO", reserva.RES_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@USU_CODIGO", reserva.USU_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@HAB_CODIGO", reserva.HAB_CODIGO);
+                sqlCommand.Parameters.AddWithValue("@RES_FECHA_INGRESO", reserva.RES_FECHA_INGRESO);
+                sqlCommand.Parameters.AddWithValue("@RES_FECHA_SALIDA", reserva.RES_FECHA_SALIDA);
+                
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
+
+        [HttpDelete]
+        public IHttpActionResult Eliminar(int id)
+        {
+            if (id < 1)
+                return BadRequest();
+            if (EliminarReserva(id))
+                return Ok();
+            else return InternalServerError();
+        }
+
+        private bool EliminarReserva(int id)
+        {
+            bool resultado = false;
+
+            using (SqlConnection sqlConnection = new SqlConnection(ConfigurationManager.ConnectionStrings["RESERVAS"].ConnectionString))
+            {
+                SqlCommand sqlCommand = new SqlCommand(@"DELETE RESERVA 
+                                                       WHERE RES_CODIGO = @RES_CODIGO", sqlConnection);
+                sqlCommand.Parameters.AddWithValue("@RES_CODIGO", id);
+
+
+                sqlConnection.Open();
+
+                int filasAfectadas = sqlCommand.ExecuteNonQuery();
+                if (filasAfectadas > 0)
+                    return true;
+
+                sqlConnection.Close();
+            }
+
+            return resultado;
+        }
     }
 }
